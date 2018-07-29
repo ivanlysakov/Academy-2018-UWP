1ff0bcf8bd73eecb4f8aa94e12c11d6c66cf8698
Ivan <ivanlysakov@gmail.com>
2018-07-29 22:22:53 +0300

    add int converter

---
+++ AirportClient/AirportClient/Convetors/IntConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace AirportClient.Convetors
+{
+    public class IntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            int data;
+
+            if (string.IsNullOrEmpty((string)value) || !int.TryParse((string)value, out data))
+            {
+                return null;
+            }
+            else
+            {
+                return data;
+            }
+        }
+    }
+}
